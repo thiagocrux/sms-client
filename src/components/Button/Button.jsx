7e15dc1ce638fe045ae20a4e822e8d9ca9d21b6d
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2021-05-13 17:21:17 -0300

    Update on the button layout

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,18 +3,11 @@ import React from 'react';
 import style from './Button.module.css';
 
 export default function Button(props) {
-  // FIXME: Change to the css module aproach.
-  const conditionalStyling = {
-    color: '#FFFFFF',
-    backgroundColor: props.action === 'submit' ? '#41436A' : '#984063',
-  };
-
   return (
     <button
       type={props.type}
       onClick={() => props.click(props.action)}
-      style={conditionalStyling}
-      className={style.button}
+      className={`${style.button} ${props.action === 'submit' ? style.continue : style.cancel}`}
     >
       {props.children}
     </button>
