21dfd851f4aff8af1296988fb870fabcffd3d83e
Matheus <matheuscruzhen@gmail.com>
2021-06-01 22:22:24 -0300

    refactor(search form component): move state out of the component

--- src/components/Patients/SearchForm/SearchForm.jsx
+++ src/components/Patients/SearchForm/SearchForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Search } from 'react-bootstrap-icons';
 
 import Field from '../../Layout/Form/Field/Field';
@@ -6,47 +6,50 @@ import Heading from '../../Layout/Heading/Heading';
 
 import style from './SearchForm.module.css';
 
-export default function PatientSearch(props) {
-  const [criterion, setCriterion] = useState('sus-card-number');
-
-  function handleSearchCriterionInputValue(event) {
-    setCriterion(event.target.value);
-  }
+export default function PatientSearch({ handleSubmit, search, setSearch }) {
+  const { criterion, inputValue } = search;
 
   return (
     <div className={style['patient-search']}>
-      <Heading type="primary">Localize o paciente que será notificado</Heading>
+      <Heading type='primary'>Localize o paciente que será notificado</Heading>
       <div className={style['grid-container']}>
         <Field>
-          <label htmlFor="search-criterion">Critério de pesquisa</label>
+          <label htmlFor='search-criterion'>Critério de pesquisa</label>
           <select
-            name="search-criterion"
+            value={criterion}
+            name='search-criterion'
             className={style['search-criterion']}
-            onChange={handleSearchCriterionInputValue}
+            onChange={event =>
+              setSearch({ ...search, criterion: event.currentTarget.value })
+            }
           >
-            <option value="sus-card-number">Número do cartão do SUS</option>
-            <option value="cpf">Número do CPF</option>
-            <option value="name">Nome do paciente</option>
+            <option value='susCardNumber'>Número do cartão do SUS</option>
+            <option value='cpf'>Número do CPF</option>
+            <option value='name'>Nome do paciente</option>
           </select>
         </Field>
         <Field>
-          <label htmlFor="search-input">&nbsp;</label>
+          <label htmlFor='search-input'>&nbsp;</label>
           <input
-            name="search-input"
-            type="text"
+            value={inputValue}
+            name='search-input'
+            type='text'
             placeholder={
-              criterion === 'sus-card-number'
+              criterion === 'susCardNumber'
                 ? 'Insira o número do cartão do SUS do paciente'
                 : criterion === 'cpf'
                 ? 'Insira o CPF do paciente'
                 : 'Insira o nome do paciente'
             }
             className={style['search-input']}
+            onChange={event =>
+              setSearch({ ...search, inputValue: event.currentTarget.value })
+            }
           />
         </Field>
       </div>
       <div className={style['flex-container']}>
-        <button className={style.button}>
+        <button className={style.button} onClick={handleSubmit}>
           <Search className={style.icon} />
           Pesquisar
         </button>
