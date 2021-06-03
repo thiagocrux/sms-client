32db004cb47d3dff5ffd92f3eac17bc3c9d0d883
Matheus <matheuscruzhen@gmail.com>
2021-06-03 15:57:22 -0300

    feat(notification container): implement conditional rendering

--- src/containers/Notification/Notification.jsx
+++ src/containers/Notification/Notification.jsx
@@ -8,18 +8,20 @@ import List from '../../components/Patients/List/List';
 import { patientsContext } from '../../context/patientsContext.js';
 
 import style from './Notification.module.css';
+import { useHistory, useLocation, useParams } from 'react-router';
+import SelectedPatientInfo from '../../components/SelectedPatientInfo/SelectedPatientInfo';
 
 export default function Notification() {
   /* TODO:
-    1. De início, ao buscar o paciente, o componente <SearchForm /> deve ser o único à mostra
-      1.1 Se a busca der algum resultado, mostrar a lista de pacientes;
-      1.2 Se a busca não der resultado, mostrar mensagem (Ex.: "Nenhum resultado foi encontrado") e um <Link /> para a criação de um novo paciente;
-    2. Escolher na lista o paciente procurado a partir dos parâmetros de busca
-      2.1 A lista deve conter o número do cartão do SUS, o nome do paciente e mais qualquer informação pertinente para cada um dos pacientes encontrados;
-      2.2 (Checar viabilidade) Onde as informações completas do paciente são mostradas, pode haver um link para a edição ou visualização de demais informações relacionadas ao paciente;
-      2.3 Ao selecionar um paciente, a caixa de pesquisa e a lista devem ser ocultadas e dar lugar a um componente com as informações do paciente selecionado (componente <Info />), a opção de pesquisar por outro paciente (um botão) e as abas de notificação (componente <Tabs />);
-      2.4 Ao pesquisar por outro paciente, tudo volta ao estado do primeiro todo;
-    3. Ao clicar numa das abas de notificação, o usuário será encaminhado a sua respectiva página;
+  1. De início, ao buscar o paciente, o componente <SearchForm /> deve ser o único à mostra
+  1.1 Se a busca der algum resultado, mostrar a lista de pacientes;
+  1.2 Se a busca não der resultado, mostrar mensagem (Ex.: "Nenhum resultado foi encontrado") e um <Link /> para a criação de um novo paciente;
+  2. Escolher na lista o paciente procurado a partir dos parâmetros de busca
+  2.1 A lista deve conter o número do cartão do SUS, o nome do paciente e mais qualquer informação pertinente para cada um dos pacientes encontrados;
+  2.2 (Checar viabilidade) Onde as informações completas do paciente são mostradas, pode haver um link para a edição ou visualização de demais informações relacionadas ao paciente;
+  2.3 Ao selecionar um paciente, a caixa de pesquisa e a lista devem ser ocultadas e dar lugar a um componente com as informações do paciente selecionado (componente <Info />), a opção de pesquisar por outro paciente (um botão) e as abas de notificação (componente <Tabs />);
+  2.4 Ao pesquisar por outro paciente, tudo volta ao estado do primeiro todo;
+  3. Ao clicar numa das abas de notificação, o usuário será encaminhado a sua respectiva página;
   */
 
   const { patients } = useContext(patientsContext);
@@ -28,6 +30,10 @@ export default function Notification() {
     inputValue: '',
   });
   const [filteredPatients, setFilteredPatients] = useState([]);
+
+  const { patientID } = useParams();
+  const location = useLocation();
+  const patientInfo = { ...location.state };
 
   const handleSubmit = () => {
     const { criterion, inputValue } = search;
@@ -54,13 +60,21 @@ export default function Notification() {
 
   return (
     <div className={style.notification}>
-      <SearchForm
-        handleSubmit={handleSubmit}
-        search={search}
-        setSearch={setSearch}
-      />
-      <List patients={filteredPatients} />
-      <Tabs />
+      {patientID ? (
+        <>
+          <SelectedPatientInfo patientInfo={patientInfo} />
+          <Tabs />
+        </>
+      ) : (
+        <>
+          <SearchForm
+            handleSubmit={handleSubmit}
+            search={search}
+            setSearch={setSearch}
+          />
+          <List patients={filteredPatients} />
+        </>
+      )}
     </div>
   );
 }
