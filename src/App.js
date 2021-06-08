42b5915f3c47260b59c93fb8ce90f0cab1929811
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2021-06-08 17:32:07 -0300

    fix: update routes and paths

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 
-import ExamForm from './containers/ExamForm/ExamForm';
-import Header from './components/Layout/Header/Header';
-import HomePage from './components/Pages/HomePage/HomePage';
-import LoginPage from './containers/LoginPage/LoginPage';
-import MonitoringForm from './containers/MonitoringForm/MonitoringForm';
-import NotificationPage from './containers/NotificationPage/NotificationPage';
-import PatientForm from './containers/PatientForm/PatientForm';
-import PageNotFound from './components/Pages/PageNotFound/PageNotFound';
-import Section from './components/Layout/Section/Section';
-import TreatmentForm from './containers/TreatmentForm/TreatmentForm';
+import ExamForm from "./containers/ExamForm/ExamForm";
+import Header from "./components/Layout/Header/Header";
+import HomePage from "./components/Pages/HomePage/HomePage";
+import LoginPage from "./containers/LoginPage/LoginPage";
+import MonitoringForm from "./containers/MonitoringForm/MonitoringForm";
+import NotificationPage from "./containers/NotificationPage/NotificationPage";
+import PatientForm from "./containers/PatientForm/PatientForm";
+import PageNotFound from "./components/Pages/PageNotFound/PageNotFound";
+import Section from "./components/Layout/Section/Section";
+import TreatmentForm from "./containers/TreatmentForm/TreatmentForm";
+import UserForm from "./containers/UserForm/UserForm";
 
-import PatientsProvider from './context/patientsContext';
+import PatientsProvider from "./context/patientsContext";
 
 export default function App() {
   return (
@@ -20,52 +21,37 @@ export default function App() {
       <PatientsProvider>
         <Section>
           <Switch>
-            <Route exact path='/'>
-              <HomePage />
-            </Route>
+            <Route exact path="/" component={HomePage} />
 
-            <Route exact path='/login'>
-              <LoginPage />
-            </Route>
+            <Route exact path="/login" component={LoginPage} />
 
-            <Route path='/notifications'>
-              <NotificationPage />
-            </Route>
-            <Route path='/notifications/patients/:patientID'>
-              <NotificationPage />
-            </Route>
+            <Route exact path="/notifications" component={NotificationPage} />
+            <Route
+              exact
+              path="/notifications/patients/:patientID"
+              component={NotificationPage}
+            />
 
-            <Route exact path='/exams'>
-              <ExamForm />
-            </Route>
-            <Route path='patients/:patientID/exams/:examID'>
-              <ExamForm />
-            </Route>
+            <Route
+              path="/notifications/patients/:patientID/exams/new"
+              component={ExamForm}
+            />
 
-            <Route exact path='/monitorings'>
-              <MonitoringForm />
-            </Route>
-            <Route path='patients/:patientID/monitorings/:monitoringID'>
-              <MonitoringForm />
-            </Route>
+            <Route
+              path="/notifications/patients/:patientID/monitorings/new"
+              component={MonitoringForm}
+            />
 
-            <Route exact path='patients/:patientID/treatments'>
-              <TreatmentForm />
-            </Route>
-            <Route path='patients/:patientID/treatments/:treatmentID'>
-              <TreatmentForm />
-            </Route>
+            <Route
+              path="/notifications/patients/:patientID/treatments/new"
+              component={TreatmentForm}
+            />
 
-            <Route exact path='/patients'>
-              <PatientForm />
-            </Route>
-            <Route path='/patients/:patientID'>
-              <PatientForm />
-            </Route>
+            <Route path="/patients/:patientID/new" component={PatientForm} />
 
-            <Route>
-              <PageNotFound />
-            </Route>
+            <Route path="/users/:patientID/new" component={UserForm} />
+
+            <Route component={PageNotFound} />
           </Switch>
         </Section>
       </PatientsProvider>
--- src/components/Notifications/Tabs/Tabs.jsx
+++ src/components/Notifications/Tabs/Tabs.jsx
@@ -1,27 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { ClipboardPlus, DropletHalf, Eye } from 'react-bootstrap-icons';
+import React from "react";
+import { Link } from "react-router-dom";
+import { ClipboardPlus, DropletHalf, Eye } from "react-bootstrap-icons";
 
-import Heading from '../../Layout/Heading/Heading';
+import Heading from "../../Layout/Heading/Heading";
 
-import style from './Tabs.module.css';
+import style from "./Tabs.module.css";
 
 export default function NotificationTabs({ patient }) {
   return (
     <>
-      <Heading type='primary'>
+      <Heading type="primary">
         Que tipo de notificação você deseja criar?
       </Heading>
       <nav className={style.navigation}>
-        <Link to={`/treatments/patients/${patient._id}`} className={style.tab}>
+        <Link
+          to={`/notifications/patients/${patient._id}/treatments/new`}
+          className={style.tab}
+        >
           <ClipboardPlus className={style.icon} />
           Novo tratamento
         </Link>
-        <Link to={`/monitorings/patients/${patient._id}`} className={style.tab}>
+        <Link
+          to={`/notifications/patients/${patient._id}/monitorings/new`}
+          className={style.tab}
+        >
           <Eye className={style.icon} />
           Novo monitoramento
         </Link>
-        <Link to={`/exams/patients/${patient._id}`} className={style.tab}>
+        <Link
+          to={`/notifications/patients/${patient._id}/exams/new`}
+          className={style.tab}
+        >
           <DropletHalf className={style.icon} />
           Novo exame
         </Link>
