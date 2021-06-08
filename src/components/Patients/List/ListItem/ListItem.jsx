fdf9544c6ead6fd9348568438f851edd7f0510f0
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2021-06-08 15:17:41 -0300

    refactor: create a function to format dates

--- src/components/Patients/List/ListItem/ListItem.jsx
+++ src/components/Patients/List/ListItem/ListItem.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { PenFill, PersonLinesFill, XCircleFill } from "react-bootstrap-icons";
 import { Link } from "react-router-dom";
-
-import { formatCPF } from "../../../../utils/formatting";
+import { formatCPF, formatDate } from "../../../../utils/formatting";
 
 import style from "./ListItem.module.css";
 
 export default function ListItem({ patient }) {
-  const date = new Date(patient.birthDate);
-  const dateFormatted = Intl.DateTimeFormat("pt-BR").format(date);
-
   return (
     <Link
       to={{
@@ -23,7 +19,9 @@ export default function ListItem({ patient }) {
       <span className={style.innerDivider}>
         {patient.socialName ? patient.socialName : patient.name}
       </span>
-      <span className={style.innerDivider}>{dateFormatted}</span>
+      <span className={style.innerDivider}>
+        {formatDate(patient.birthDate)}
+      </span>
       <span className={style.controls}>
         <PersonLinesFill className={style.icon} />
         <PenFill className={style.icon} />
--- src/utils/formatting.js
+++ src/utils/formatting.js
@@ -1,4 +1,10 @@
 export function formatCPF(cpf) {
-  cpf = cpf.replace(/[^\d]/g, '');
-  return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+  cpf = cpf.replace(/[^\d]/g, "");
+  return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+}
+
+export function formatDate(dateAsString) {
+  const date = new Date(dateAsString);
+  const dateFormatted = Intl.DateTimeFormat("pt-BR").format(date);
+  return dateFormatted;
 }
