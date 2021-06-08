db23aea5be6b0e58320c5820729b7028f19bb602
Matheus <matheuscruzhen@gmail.com>
2021-06-08 17:28:58 -0300

    feat: add a formatting sus number function

--- src/utils/formatting.js
+++ src/utils/formatting.js
@@ -1,4 +1,9 @@
 export function formatCPF(cpf) {
-  cpf = cpf.replace(/[^\d]/g, '');
-  return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+  cpf = cpf.replace(/[^\d]/g, "");
+  return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+}
+
+export function formatSUS(susCardNumber) {
+  susCardNumber = susCardNumber.replace(/[^\d]/g, "");
+  return susCardNumber.replace(/(\d{3})(\d{4})(\d{4})(\d{4})/, "$1 $2 $3 $4");
 }
