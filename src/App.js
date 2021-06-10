0367aecb51cfca7efc6202c79522082929290a68
Matheus <matheuscruzhen@gmail.com>
2021-06-10 00:06:01 -0300

    refactor: create route component

--- src/App.js
+++ src/App.js
@@ -1,63 +1,15 @@
-import { Route, Switch } from "react-router-dom";
-
-import ExamForm from "./containers/ExamForm/ExamForm";
+import React from "react";
 import Header from "./components/Layout/Header/Header";
-import HomePage from "./components/Pages/HomePage/HomePage";
-import LoginPage from "./containers/LoginPage/LoginPage";
-import MonitoringForm from "./containers/MonitoringForm/MonitoringForm";
-import NotificationPage from "./containers/NotificationPage/NotificationPage";
-import PatientForm from "./containers/PatientForm/PatientForm";
-import PageNotFound from "./components/Pages/PageNotFound/PageNotFound";
 import Section from "./components/Layout/Section/Section";
-import TreatmentForm from "./containers/TreatmentForm/TreatmentForm";
-import UserForm from "./containers/UserForm/UserForm";
-
-import PatientsProvider from "./context/patientsContext";
-import PatientPage from "./containers/PatientPage/PatientPage";
+import Routes from "./routes/Routes";
 
 export default function App() {
   return (
     <>
       <Header />
-      <PatientsProvider>
-        <Section>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-
-            <Route exact path="/login" component={LoginPage} />
-
-            <Route exact path="/notifications" component={NotificationPage} />
-            <Route
-              exact
-              path="/notifications/patients/:patientID"
-              component={NotificationPage}
-            />
-
-            <Route
-              path="/notifications/patients/:patientID/exams/new"
-              component={ExamForm}
-            />
-
-            <Route
-              path="/notifications/patients/:patientID/monitorings/new"
-              component={MonitoringForm}
-            />
-
-            <Route
-              path="/notifications/patients/:patientID/treatments/new"
-              component={TreatmentForm}
-            />
-
-            <Route path="/patients" component={PatientPage} />
-
-            <Route path="/patients/:patientID/new" component={PatientForm} />
-
-            <Route path="/users/:patientID/new" component={UserForm} />
-
-            <Route component={PageNotFound} />
-          </Switch>
-        </Section>
-      </PatientsProvider>
+      <Section>
+        <Routes />
+      </Section>
     </>
   );
 }
