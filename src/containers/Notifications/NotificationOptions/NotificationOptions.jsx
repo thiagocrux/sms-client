654aa37564fa38471991a039260d6b12de5e65cc
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2021-06-12 21:26:25 -0300

    refactor: remove some lines and change the way to get the patient id

--- src/containers/Notifications/NotificationOptions/NotificationOptions.jsx
+++ src/containers/Notifications/NotificationOptions/NotificationOptions.jsx
@@ -1,36 +1,27 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { ClipboardPlus, DropletHalf, Eye } from "react-bootstrap-icons";
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { ClipboardPlus, DropletHalf, Eye } from 'react-bootstrap-icons';
 
-import Heading from "../../../components/Common/Heading/Heading";
+import Heading from '../../../components/Common/Heading/Heading';
 
-import style from "./NotificationOptions.module.css";
+import style from './NotificationOptions.module.css';
 
-export default function NotificationTabs({ patient }) {
+export default function NotificationOptions() {
+  const { patientID } = useParams();
+
   return (
     <>
-      <Heading type="primary">
-        Que tipo de notificação você deseja criar?
-      </Heading>
+      <Heading type="primary">Que tipo de notificação você deseja criar?</Heading>
       <nav className={style.navigation}>
-        <Link
-          to={`/notifications/patients/${patient._id}/treatments/new`}
-          className={style.tab}
-        >
+        <Link to={`/notifications/patients/${patientID}/treatments/new`} className={style.tab}>
           <ClipboardPlus className={style.icon} />
           Novo tratamento
         </Link>
-        <Link
-          to={`/notifications/patients/${patient._id}/monitorings/new`}
-          className={style.tab}
-        >
+        <Link to={`/notifications/patients/${patientID}/monitorings/new`} className={style.tab}>
           <Eye className={style.icon} />
           Novo monitoramento
         </Link>
-        <Link
-          to={`/notifications/patients/${patient._id}/exams/new`}
-          className={style.tab}
-        >
+        <Link to={`/notifications/patients/${patientID}/exams/new`} className={style.tab}>
           <DropletHalf className={style.icon} />
           Novo exame
         </Link>
