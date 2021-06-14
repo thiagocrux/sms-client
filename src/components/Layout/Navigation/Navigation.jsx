cc38ea2c859729a80c0b4440e1b8e547b55c098f
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2021-06-14 17:46:42 -0300

    style: change navigation highlight colors

--- src/components/Layout/Navigation/Navigation.jsx
+++ src/components/Layout/Navigation/Navigation.jsx
@@ -1,58 +1,38 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import {
-  ClipboardData,
-  House,
-  JournalMedical,
-  PeopleFill,
-} from 'react-bootstrap-icons';
+import { ClipboardData, House, JournalMedical, PeopleFill } from 'react-bootstrap-icons';
 
 import style from './Navigation.module.css';
 
 export default function Navigation() {
   /* FIXME: Ao definir padrão do layout, definir a cor correta */
   const activeStyle = {
-    color: '#1976d2',
-    borderBottom: '.1rem solid #1976d2',
+    color: '#809bce',
+    borderBottom: '.1rem solid #809bce',
   };
 
   return (
     <nav className={style.navigation}>
       <div className={style['div-container']}>
-        <NavLink exact to='/' className={style.link} activeStyle={activeStyle}>
+        <NavLink exact to="/" className={style.link} activeStyle={activeStyle}>
           <House className={style.icon} />
           Página inicial
         </NavLink>
       </div>
       <div className={style['div-container']}>
-        <NavLink
-          exact
-          to='/patients'
-          className={style.link}
-          activeStyle={activeStyle}
-        >
+        <NavLink exact to="/patients" className={style.link} activeStyle={activeStyle}>
           <PeopleFill className={style.icon} />
           Pacientes
         </NavLink>
       </div>
       <div className={style['div-container']}>
-        <NavLink
-          exact
-          to='/notifications'
-          className={style.link}
-          activeStyle={activeStyle}
-        >
+        <NavLink exact to="/notifications" className={style.link} activeStyle={activeStyle}>
           <JournalMedical className={style.icon} />
           Notificações
         </NavLink>
       </div>
       <div className={style['div-container']}>
-        <NavLink
-          exact
-          to='/reports'
-          className={style.link}
-          activeStyle={activeStyle}
-        >
+        <NavLink exact to="/reports" className={style.link} activeStyle={activeStyle}>
           <ClipboardData className={style.icon} />
           Relatórios
         </NavLink>
