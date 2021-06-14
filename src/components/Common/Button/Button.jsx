fba165f1c2e181c1b31ba7e3d3be223f11a18c2d
Matheus <matheuscruzhen@gmail.com>
2021-06-14 18:21:34 -0300

    fix: fix button prop and patient selected delete function wrong call

--- src/components/Common/Button/Button.jsx
+++ src/components/Common/Button/Button.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 import style from './Button.module.css';
 
-export default function Button({ handler, action, type, children }) {
+export default function Button({ click, action, type, children }) {
   return (
     <button
       type={type}
-      onClick={handler}
+      onClick={click}
       className={`${style.button} ${
         action === 'submit'
           ? style.submit
