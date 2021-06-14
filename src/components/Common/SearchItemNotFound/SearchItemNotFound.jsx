13f8008f87d6aace00856dc0ac00fa6c2dc20dc5
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2021-06-14 17:18:52 -0300

    style: modify the component structure and style

--- src/components/Common/SearchItemNotFound/SearchItemNotFound.jsx
+++ src/components/Common/SearchItemNotFound/SearchItemNotFound.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ExclamationCircle } from 'react-bootstrap-icons';
+import { ExclamationCircle, PersonPlusFill } from 'react-bootstrap-icons';
 
 import style from './SearchItemNotFound.module.css';
 
 export default function SearchItemNotFound(props) {
   return (
-    <div className={style['search-item-not-found']}>
+    <div className={style.messageBox}>
       <p>
         <ExclamationCircle className={style.icon} />A busca não encontrou resultados. Se deseja
-        cadastrar um novo {props.subject}&nbsp;<Link to={props.link}>clique aqui</Link>.
+        cadastrar um novo paciente clique no botão abaixo.
       </p>
+      <Link to={props.link}>
+        <button className={`${style.button} ${style.infoButton}`}>
+          Cadastrar paciente
+          <PersonPlusFill className={style.icon} />
+        </button>
+      </Link>
     </div>
   );
 }
