024b4c8d3760bdb9d7bb120d046542f302756cb2
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2021-06-13 22:05:14 -0300

    refactor: create a component to replace the controls

--- src/containers/Patients/PatientListItem/PatientListItem.jsx
+++ src/containers/Patients/PatientListItem/PatientListItem.jsx
@@ -1,38 +1,30 @@
 import React from 'react';
-import { PenFill, PersonLinesFill, TrashFill } from 'react-bootstrap-icons';
 import { Link, useLocation } from 'react-router-dom';
-
 import { formatCPF, formatDate, formatSUSCardNumber } from '../../../utils/dataFormatter';
+
+import PatientControls from '../PatientControls/PatientControls';
 
 import style from './PatientListItem.module.css';
 
 export default function PatientListItem({ patient }) {
   const { _id, susCardNumber, cpf, name, socialName, birthDate } = patient;
-  const { listItem, innerDivider, icon, controls } = style;
-
   const location = useLocation();
   const url = location.pathname === '/patients' ? '' : '/notifications';
 
-  console.log('[LIST ITEM]: ' + patient._id);
-
   return (
     <Link
+      className={style.listItem}
       patient={patient}
       to={{
         pathname: `${url}/patients/${_id}/`,
         state: patient,
       }}
-      className={listItem}
     >
-      <span className={innerDivider}>{formatSUSCardNumber(susCardNumber)}</span>
-      <span className={innerDivider}>{formatCPF(cpf)}</span>
-      <span className={innerDivider}>{socialName ? socialName : name}</span>
-      <span className={innerDivider}>{formatDate(birthDate)}</span>
-      <span className={controls}>
-        <PersonLinesFill className={icon} />
-        <PenFill className={icon} />
-        <TrashFill className={icon} />
-      </span>
+      <span className={style.innerDivider}>{formatSUSCardNumber(susCardNumber)}</span>
+      <span className={style.innerDivider}>{formatCPF(cpf)}</span>
+      <span className={style.innerDivider}>{socialName ? socialName : name}</span>
+      <span className={style.innerDivider}>{formatDate(birthDate)}</span>
+      <PatientControls patientID={patient._id} />
     </Link>
   );
 }
