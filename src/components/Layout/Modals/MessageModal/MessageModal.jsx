e56daaa55e0c1140ac3c1ca6b55b42258abc2a68
Matheus <matheuscruzhen@gmail.com>
2021-06-13 23:35:59 -0300

    feat: create message modal

---
+++ src/components/Layout/Modals/MessageModal/MessageModal.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import style from './ConfirmationModal.module.css';
+
+function MessageModal({ open, header, body, closeModal }) {
+  if (!open) return null;
+
+  return ReactDOM.createPortal(
+    <div className={style.container}>
+      <div className={style.modalBox}>
+        <p className={style.header}>{header}</p>
+        <p className={style.header}>{body}</p>
+        <div className={style.buttons}>
+          <button className={style.button} onClick={closeModal}>
+            Sim
+          </button>
+        </div>
+      </div>
+    </div>,
+    document.getElementById('messagePortal')
+  );
+}
+
+export default MessageModal;
