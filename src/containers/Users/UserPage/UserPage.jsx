9e6807ada965cce8281eba12259ca6c843ca229e
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2021-06-18 17:29:09 -0300

    feat: create the user page

---
+++ src/containers/Users/UserPage/UserPage.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+
+import UserList from '../../Users/UserList/UserList';
+import UserSearchForm from '../../Users/UserSearchForm/UserSearchForm';
+
+import api from '../../../utils/api';
+
+import style from './UserPage.module.css';
+
+export default function PatientPage() {
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [search, setSearch] = useState({
+    criterion: 'cpf',
+    inputValue: '',
+  });
+
+  useEffect(() => {
+    api.get('/users/').then((response) => {
+      setUsers(response.data.users);
+    });
+  }, []);
+
+  useEffect(() => {
+    const { criterion, inputValue } = search;
+
+    const filter = users.filter((filteredUsers) => {
+      const key = Object.keys(filteredUsers).filter((key) => key === criterion);
+      return filteredUsers[key]
+        .toLowerCase()
+        .includes(inputValue.toLowerCase());
+    });
+
+    setFilteredUsers(filter);
+  }, [users, search]);
+
+  const handleSubmit = () => {
+    const { criterion, inputValue } = search;
+
+    const filteredUser = users.filter((user) => {
+      const key = Object.keys(user).filter((key) => key === criterion);
+      return user[key].toLowerCase() === inputValue.toLowerCase();
+    });
+    setFilteredUsers(filteredUser);
+  };
+
+  return (
+    <div className={style.patient}>
+      <>
+        <UserSearchForm
+          formHeader="Localize o usuário que deseja consultar"
+          handleSubmit={handleSubmit}
+          search={search}
+          setSearch={setSearch}
+        />
+        <UserList filteredResult={filteredUsers} />
+      </>
+    </div>
+  );
+}
