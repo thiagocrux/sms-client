e2d298b3a44151b14f78dd81fd6c43cb7811399e
Thiago Cruz <29850573+thiagocrux@users.noreply.github.com>
2021-07-01 22:26:14 -0300

    feat: add default callback for button return

--- src/components/Common/Buttons/ButtonReturn/ButtonReturn.jsx
+++ src/components/Common/Buttons/ButtonReturn/ButtonReturn.jsx
@@ -8,7 +8,10 @@ export default function ButtonReturn(props) {
   const history = useHistory();
 
   return (
-    <button className={style.button} onClick={() => history.push(props.link)}>
+    <button
+      className={style.button}
+      onClick={props.link ? () => history.push(props.link) : history.goBack()}
+    >
       <ArrowLeft className={style.icon} />
     </button>
   );
